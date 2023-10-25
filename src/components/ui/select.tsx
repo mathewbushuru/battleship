cb4cf62769b8ff8141760d93290a566519e95746
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-10-25 11:52:36 -0700

    select component

---
+++ src/components/ui/select.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { Check, ChevronDown } from "lucide-react";
+
+import { cn } from "@/lib/ui-utils";
+
+const Select = SelectPrimitive.Root;
+
+const SelectValue = SelectPrimitive.Value;
+
+const SelectTrigger = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <SelectPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        "bg-background ring-offset-background flex h-10 w-full items-center justify-between rounded-md border border-stone-800 px-3 py-2 focus:outline-none",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <SelectPrimitive.Icon asChild>
+        <ChevronDown className="h-4 w-4" />
+      </SelectPrimitive.Icon>
+    </SelectPrimitive.Trigger>
+  );
+});
+
+const SelectContent = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({ className, children, position = "popper", ...props }, ref) => {
+  return (
+    <SelectPrimitive.Portal>
+      <SelectPrimitive.Content
+        ref={ref}
+        className={cn(
+          "relative z-50 min-w-[8rem] rounded-md border bg-white shadow-md",
+          className,
+        )}
+        position={position}
+        {...props}
+      >
+        <SelectPrimitive.Viewport
+          className={cn(
+            "p-1",
+            position === "popper" &&
+              "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
+          )}
+        >
+          {children}
+        </SelectPrimitive.Viewport>
+      </SelectPrimitive.Content>
+    </SelectPrimitive.Portal>
+  );
+});
+
+const SelectItem = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <SelectPrimitive.Item
+      ref={ref}
+      className={cn(
+        "focus:bg-background/50 relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        className,
+      )}
+      {...props}
+    >
+      <span className="absolute left-2 h-3.5 w-3.5">
+        <SelectPrimitive.ItemIndicator>
+          <Check className="h-4 w-4" />
+        </SelectPrimitive.ItemIndicator>
+      </span>
+      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+    </SelectPrimitive.Item>
+  );
+});
+
+export {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+};
--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,6 +1,13 @@
 import Header from "@/components/header";
 import Blockquote from "@/components/ui/blockquote";
 import Button from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import quotes from "@/lib/quotes";
 
@@ -10,14 +17,37 @@ function WelcomePage() {
   return (
     <div
       data-testid="WelcomePage"
-      className="flex flex-col justify-between space-y-4 px-6 py-16 h-full"
+      className="flex h-full flex-col justify-between space-y-4 px-6 py-16"
     >
       <Header />
       <Blockquote
         quote={quotes.start[chosenStartQuote].quote}
         author={quotes.start[chosenStartQuote].author}
       />
-      <Button size="lg" className="sm:max-w-sm sm:mx-auto">Enter Battle</Button>
+      <div className="space-y-4">
+        <div className="flex items-center justify-center gap-2">
+          Your rank is:
+          <Select defaultValue="lieutenant">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="lieutenant">Lieutenant</SelectItem>
+              <SelectItem value="commander">Commander</SelectItem>
+              <SelectItem value="captain" disabled>Captain</SelectItem>
+              <SelectItem value="viceadmiral" disabled>Vice Admiral</SelectItem>
+              <SelectItem value="admiral" disabled>Admiral</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <p className="text-sm text-center font-mono">
+          Win more battles to unlock higher difficulties
+        </p>
+      </div>
+
+      <Button size="lg" className="sm:mx-auto sm:max-w-sm">
+        Enter Battle
+      </Button>
     </div>
   );
 }
