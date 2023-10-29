25eb0da40cfeb8971f12673b20429d2340dd90a3
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-10-29 09:44:13 -0700

    highlight cell on mouseover during ship placement

--- src/components/board-grid.tsx
+++ src/components/board-grid.tsx
@@ -1,35 +1,80 @@
+import { useState } from "react";
+import { type ClassValue } from "clsx";
+
 import { cn } from "@/lib/ui-utils";
 
-function BoardCell({ row, col }: { row: number; col: number }) {
+function BoardCell({
+  row,
+  col,
+  currentShipColorClass,
+}: {
+  row: number;
+  col: number;
+  currentShipColorClass: ClassValue;
+}) {
+  const [isMouseOver, setIsMouseOver] = useState(false);
   console.log(row, col);
+
   return (
-    <div className="border-secondary m-0.5 h-8 w-8 rounded-sm border sm:h-11 sm:w-11">
+    <div
+      className={cn(
+        "border-secondary m-0.5 h-8 w-8 rounded-sm border sm:h-11 sm:w-11",
+        isMouseOver && `${currentShipColorClass}`,
+      )}
+      onMouseEnter={() => setIsMouseOver(true)}
+      onMouseLeave={() => setIsMouseOver(false)}
+    >
       {" "}
     </div>
   );
 }
 
-function BoardRow({ row }: { row: number }) {
+function BoardRow({
+  row,
+  currentShipColorClass,
+}: {
+  row: number;
+  currentShipColorClass: ClassValue;
+}) {
   return (
     <div className="flex">
       {Array(10)
         .fill(null)
         .map((_, colIndex) => {
-          return <BoardCell row={row} col={colIndex} key={colIndex} />;
+          return (
+            <BoardCell
+              key={colIndex}
+              row={row}
+              col={colIndex}
+              currentShipColorClass={currentShipColorClass}
+            />
+          );
         })}
     </div>
   );
 }
 
-interface boardGridProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface boardGridProps extends React.HTMLAttributes<HTMLDivElement> {
+  currentShipColorClass: ClassValue;
+}
 
-export default function BoardGrid({ className, ...props }: boardGridProps) {
+export default function BoardGrid({
+  className,
+  currentShipColorClass,
+  ...props
+}: boardGridProps) {
   return (
     <div className={cn("flex flex-col items-center", className)} {...props}>
       {Array(10)
         .fill(null)
         .map((_, rowIndex) => {
-          return <BoardRow row={rowIndex} key={rowIndex} />;
+          return (
+            <BoardRow
+              key={rowIndex}
+              row={rowIndex}
+              currentShipColorClass={currentShipColorClass}
+            />
+          );
         })}
     </div>
   );
--- src/pages/placement-page.tsx
+++ src/pages/placement-page.tsx
@@ -4,11 +4,11 @@ import BoardGrid from "@/components/board-grid";
 
 function PlacementPage() {
   const allShips = [
-    "Carrier",
-    "Battleship",
-    "Destroyer",
-    "Submarine",
-    "Patroller",
+    { name: "Carrier", shipColorClass: "bg-amber-500" },
+    { name: "Battleship", shipColorClass: "bg-teal-500" },
+    { name: "Destroyer", shipColorClass: "bg-cyan-500" },
+    { name: "Submarine", shipColorClass: "bg-indigo-500" },
+    { name: "Patroller", shipColorClass: "bg-rose-500" },
   ];
   const placedShips: string[] = [];
   const currentShip = allShips.shift();
@@ -17,46 +17,46 @@ function PlacementPage() {
     <div className="px-6 py-6">
       <Header />
       <p className="mt-4 text-center text-lg">
-        Lieutenant, place your {currentShip}
+        Lieutenant, place your {currentShip?.name}
       </p>
       <div className="mt-4 flex flex-wrap justify-center gap-y-2 sm:justify-between sm:px-12 lg:justify-center lg:gap-x-12">
         <ShipSelectorIcon
           name="Carrier"
           cells={5}
-          isActive={currentShip === "Carrier"}
-          isOnBoard={placedShips.some(ship => ship==="Carrier")}
-          shipColorClass="bg-blue-500"
+          isActive={currentShip?.name === "Carrier"}
+          isOnBoard={placedShips.some((ship) => ship === "Carrier")}
+          shipColorClass="bg-amber-500"
         />
         <ShipSelectorIcon
           name="Battleship"
           cells={4}
-          isActive={currentShip === "Battleship"}
-          isOnBoard={placedShips.some(ship => ship==="Battleship")}
+          isActive={currentShip?.name === "Battleship"}
+          isOnBoard={placedShips.some((ship) => ship === "Battleship")}
           shipColorClass="bg-teal-500"
         />
         <ShipSelectorIcon
           name="Destroyer"
           cells={3}
-          isActive={currentShip === "Destroyer"}
-          isOnBoard={placedShips.some(ship => ship==="Destroyer")}
+          isActive={currentShip?.name === "Destroyer"}
+          isOnBoard={placedShips.some((ship) => ship === "Destroyer")}
           shipColorClass="bg-cyan-500"
         />
         <ShipSelectorIcon
           name="Submarine"
           cells={3}
-          isActive={currentShip === "Submarine"}
-          isOnBoard={placedShips.some(ship => ship==="Submarine")}
+          isActive={currentShip?.name === "Submarine"}
+          isOnBoard={placedShips.some((ship) => ship === "Submarine")}
           shipColorClass="bg-indigo-500"
         />
         <ShipSelectorIcon
           name="Patroller"
           cells={2}
-          isActive={currentShip === "Patroller"}
-          isOnBoard={placedShips.some(ship => ship==="Patroller")}
+          isActive={currentShip?.name === "Patroller"}
+          isOnBoard={placedShips.some((ship) => ship === "Patroller")}
           shipColorClass="bg-rose-500"
         />
       </div>
-      <BoardGrid className="mt-8" />
+      <BoardGrid className="mt-8" currentShipColorClass={currentShip?.shipColorClass} />
     </div>
   );
 }
