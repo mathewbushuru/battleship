9f9bfc87a5c9e522b52259a94c10b138792eb4d8
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-13 22:01:18 -0800

    vary direction of automatic ship placement

--- src/components/enemy-board-grid.tsx
+++ src/components/enemy-board-grid.tsx
@@ -29,109 +29,224 @@ export default function EnemyBoardGrid({
     ) as ShipState["shipData"];
 
     const allEnemyCells: number[][] = [];
-    let row: number, col: number;
+    let row: number, col: number, direction: "row" | "column";
 
-    row = getRandomInt(9);
-    col = getRandomInt(6);
-    const enemyCarrierCells = [
-      [row, col],
-      [row, col + 1],
-      [row, col + 2],
-      [row, col + 3],
-      [row, col + 4],
-    ];
+    // enemy carrier cells
+    direction = getRandomInt(2) === 1 ? "row" : "column";
+    let enemyCarrierCells;
+    if (direction === "row") {
+      row = getRandomInt(10);
+      col = getRandomInt(6);
+      enemyCarrierCells = [
+        [row, col],
+        [row, col + 1],
+        [row, col + 2],
+        [row, col + 3],
+        [row, col + 4],
+      ];
+    } else {
+      row = getRandomInt(6);
+      col = getRandomInt(10);
+      enemyCarrierCells = [
+        [row, col],
+        [row + 1, col],
+        [row + 2, col],
+        [row + 3, col],
+        [row + 4, col],
+      ];
+    }
     allEnemyCells.push(...enemyCarrierCells);
     updatedShipData.carrier.occupiedCells = enemyCarrierCells;
     updatedShipData.carrier.alreadyPlaced = true;
 
-    row = getRandomInt(10);
-    col = getRandomInt(7);
-    while (
-      allEnemyCells.some((el) => {
-        return (
-          (el[0] === row && el[1] === col) ||
-          (el[0] === row && el[1] === col + 1) ||
-          (el[0] === row && el[1] === col + 2) ||
-          (el[0] === row && el[1] === col + 3)
-        );
-      })
-    ) {
+    // enemy battleship cells
+    direction = getRandomInt(2) === 1 ? "row" : "column";
+    let enemyBattleshipCells;
+    if (direction === "row") {
       row = getRandomInt(10);
       col = getRandomInt(7);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row && el[1] === col + 1) ||
+            (el[0] === row && el[1] === col + 2) ||
+            (el[0] === row && el[1] === col + 3)
+          );
+        })
+      ) {
+        row = getRandomInt(10);
+        col = getRandomInt(7);
+      }
+      enemyBattleshipCells = [
+        [row, col],
+        [row, col + 1],
+        [row, col + 2],
+        [row, col + 3],
+      ];
+    } else {
+      row = getRandomInt(7);
+      col = getRandomInt(10);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row + 1 && el[1] === col) ||
+            (el[0] === row + 2 && el[1] === col) ||
+            (el[0] === row + 3 && el[1] === col)
+          );
+        })
+      ) {
+        row = getRandomInt(7);
+        col = getRandomInt(10);
+      }
+      enemyBattleshipCells = [
+        [row, col],
+        [row + 1, col],
+        [row + 2, col],
+        [row + 3, col],
+      ];
     }
-    const enemyBattleshipCells = [
-      [row, col],
-      [row, col + 1],
-      [row, col + 2],
-      [row, col + 3],
-    ];
     allEnemyCells.push(...enemyBattleshipCells);
     updatedShipData.battleship.occupiedCells = enemyBattleshipCells;
     updatedShipData.battleship.alreadyPlaced = true;
 
-    row = getRandomInt(10);
-    col = getRandomInt(8);
-    while (
-      allEnemyCells.some((el) => {
-        return (
-          (el[0] === row && el[1] === col) ||
-          (el[0] === row && el[1] === col + 1) ||
-          (el[0] === row && el[1] === col + 2)
-        );
-      })
-    ) {
+    // enemy destroyer cells
+    direction = getRandomInt(2) === 1 ? "row" : "column";
+    let enemyDestroyerCells;
+    if (direction === "row") {
       row = getRandomInt(10);
       col = getRandomInt(8);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row && el[1] === col + 1) ||
+            (el[0] === row && el[1] === col + 2)
+          );
+        })
+      ) {
+        row = getRandomInt(10);
+        col = getRandomInt(8);
+      }
+      enemyDestroyerCells = [
+        [row, col],
+        [row, col + 1],
+        [row, col + 2],
+      ];
+    } else {
+      row = getRandomInt(8);
+      col = getRandomInt(10);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row + 1 && el[1] === col) ||
+            (el[0] === row + 2 && el[1] === col)
+          );
+        })
+      ) {
+        row = getRandomInt(8);
+        col = getRandomInt(10);
+      }
+      enemyDestroyerCells = [
+        [row, col],
+        [row + 1, col],
+        [row + 2, col],
+      ];
     }
-    const enemyDestroyerCells = [
-      [row, col],
-      [row, col + 1],
-      [row, col + 2],
-    ];
     allEnemyCells.push(...enemyDestroyerCells);
     updatedShipData.destroyer.occupiedCells = enemyDestroyerCells;
     updatedShipData.destroyer.alreadyPlaced = true;
 
-    row = getRandomInt(10);
-    col = getRandomInt(8);
-    while (
-      allEnemyCells.some((el) => {
-        return (
-          (el[0] === row && el[1] === col) ||
-          (el[0] === row && el[1] === col + 1) ||
-          (el[0] === row && el[1] === col + 2)
-        );
-      })
-    ) {
+    // enemy submarine cells
+    direction = getRandomInt(2) === 1 ? "row" : "column";
+    let enemySubmarineCells;
+    if (direction === "row") {
       row = getRandomInt(10);
       col = getRandomInt(8);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row && el[1] === col + 1) ||
+            (el[0] === row && el[1] === col + 2)
+          );
+        })
+      ) {
+        row = getRandomInt(10);
+        col = getRandomInt(8);
+      }
+      enemySubmarineCells = [
+        [row, col],
+        [row, col + 1],
+        [row, col + 2],
+      ];
+    } else {
+      row = getRandomInt(8);
+      col = getRandomInt(10);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row + 1 && el[1] === col) ||
+            (el[0] === row + 2 && el[1] === col)
+          );
+        })
+      ) {
+        row = getRandomInt(8);
+        col = getRandomInt(10);
+      }
+      enemySubmarineCells = [
+        [row, col],
+        [row + 1, col],
+        [row + 2, col],
+      ];
     }
-    const enemySubmarineCells = [
-      [row, col],
-      [row, col + 1],
-      [row, col + 2],
-    ];
     allEnemyCells.push(...enemySubmarineCells);
     updatedShipData.submarine.occupiedCells = enemySubmarineCells;
     updatedShipData.submarine.alreadyPlaced = true;
 
-    row = getRandomInt(10);
-    col = getRandomInt(9);
-    while (
-      allEnemyCells.some((el) => {
-        return (
-          (el[0] === row && el[1] === col) ||
-          (el[0] === row && el[1] === col + 1)
-        );
-      })
-    ) {
+    // enemy patroller cells
+    direction = getRandomInt(2) === 1 ? "row" : "column";
+    let enemyPatrollerCells;
+    if (direction === "row") {
       row = getRandomInt(10);
       col = getRandomInt(9);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row && el[1] === col + 1)
+          );
+        })
+      ) {
+        row = getRandomInt(10);
+        col = getRandomInt(9);
+      }
+      enemyPatrollerCells = [
+        [row, col],
+        [row, col + 1],
+      ];
+    } else {
+      row = getRandomInt(9);
+      col = getRandomInt(10);
+      while (
+        allEnemyCells.some((el) => {
+          return (
+            (el[0] === row && el[1] === col) ||
+            (el[0] === row + 1 && el[1] === col)
+          );
+        })
+      ) {
+        row = getRandomInt(9);
+        col = getRandomInt(10);
+      }
+      enemyPatrollerCells = [
+        [row, col],
+        [row + 1, col],
+      ];
     }
-    const enemyPatrollerCells = [
-      [row, col],
-      [row, col + 1],
-    ];
     updatedShipData.patroller.occupiedCells = enemyPatrollerCells;
     updatedShipData.patroller.alreadyPlaced = true;
 
