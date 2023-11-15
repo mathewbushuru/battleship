408bf777c3a802d662e5d76dad87d09ec4f33f34
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-14 21:59:14 -0800

    successful hit shot functionality

--- src/components/enemy-board-cell.tsx
+++ src/components/enemy-board-cell.tsx
@@ -1,5 +1,4 @@
-import { Ship, CircleDashed } from "lucide-react";
-// CircleDotDashed
+import { Ship, CircleDashed, CircleDotDashed } from "lucide-react";
 
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import {
@@ -33,6 +32,29 @@ export default function EnemyBoardCell({
     dispatch(setShipDataAction(updatedShipData));
   };
 
+  const isHitCarrierCell = shipData.carrier.hitCells.some(
+    (el) => el[0] === row && el[1] === col,
+  );
+  const isHitBattleshipCell = shipData.battleship.hitCells.some(
+    (el) => el[0] === row && el[1] === col,
+  );
+  const isHitDestroyerCell = shipData.destroyer.hitCells.some(
+    (el) => el[0] === row && el[1] === col,
+  );
+  const isHitSubmarineCell = shipData.submarine.hitCells.some(
+    (el) => el[0] === row && el[1] === col,
+  );
+  const isHitPatrollerCell = shipData.patroller.hitCells.some(
+    (el) => el[0] === row && el[1] === col,
+  );
+
+  const isSuccessfullyHit =
+    isHitCarrierCell ||
+    isHitBattleshipCell ||
+    isHitDestroyerCell ||
+    isHitSubmarineCell ||
+    isHitPatrollerCell;
+
   const isCarrierCell = shipData.carrier.occupiedCells.some(
     (el) => el[0] === row && el[1] === col,
   );
@@ -52,10 +74,21 @@ export default function EnemyBoardCell({
   const isMissedHitCell = missedHitCells.some(
     (el) => el[0] === row && el[1] === col,
   );
+  const isHitCell = isSuccessfullyHit || isMissedHitCell;
 
   let cellContent: React.ReactNode = "";
 
   if (
+    isHitCarrierCell ||
+    isHitBattleshipCell ||
+    isHitDestroyerCell ||
+    isHitSubmarineCell ||
+    isHitPatrollerCell
+  ) {
+    cellContent = (
+      <CircleDotDashed className="h-4 w-4 text-primary sm:h-5 sm:w-5" />
+    );
+  } else if (
     isCarrierCell ||
     isBattleshipCell ||
     isDestroyerCell ||
@@ -69,6 +102,13 @@ export default function EnemyBoardCell({
     );
   }
 
+  const shipIsAlreadySunk =
+    (isCarrierCell && shipData.carrier.alreadySunk) ||
+    (isBattleshipCell && shipData.battleship.alreadySunk) ||
+    (isDestroyerCell && shipData.destroyer.alreadySunk) ||
+    (isSubmarineCell && shipData.submarine.alreadySunk) ||
+    (isPatrollerCell && shipData.patroller.alreadySunk);
+
   const handleShotFired = () => {
     if (
       !(
@@ -79,23 +119,61 @@ export default function EnemyBoardCell({
         isPatrollerCell
       )
     ) {
-      // misfired shot
       dispatch(addMissedHitCellAction([row, col]));
+      return;
     }
+
+    const updatedShipData = JSON.parse(
+      JSON.stringify(shipData),
+    ) as ShipState["shipData"];
+    if (isCarrierCell) {
+      updatedShipData.carrier.hitCells.push([row, col]);
+      updatedShipData.carrier.alreadySunk =
+        updatedShipData.carrier.numOfCells ===
+        updatedShipData.carrier.hitCells.length;
+    } else if (isBattleshipCell) {
+      updatedShipData.battleship.hitCells.push([row, col]);
+      updatedShipData.battleship.alreadySunk =
+        updatedShipData.battleship.numOfCells ===
+        updatedShipData.battleship.hitCells.length;
+    } else if (isDestroyerCell) {
+      updatedShipData.destroyer.hitCells.push([row, col]);
+      updatedShipData.destroyer.alreadySunk =
+        updatedShipData.destroyer.numOfCells ===
+        updatedShipData.destroyer.hitCells.length;
+    } else if (isSubmarineCell) {
+      updatedShipData.submarine.hitCells.push([row, col]);
+      updatedShipData.submarine.alreadySunk =
+        updatedShipData.submarine.numOfCells ===
+        updatedShipData.submarine.hitCells.length;
+    } else if (isPatrollerCell) {
+      updatedShipData.patroller.hitCells.push([row, col]);
+      updatedShipData.patroller.alreadySunk =
+        updatedShipData.patroller.numOfCells ===
+        updatedShipData.patroller.hitCells.length;
+    }
+    setShipData(updatedShipData);
   };
 
   return (
     <div
       className={cn(
         "opacity-85 m-[1px] flex h-7 w-7 cursor-pointer items-center justify-center rounded-sm border border-secondary bg-background text-gray-200 sm:m-0.5 sm:h-11 sm:w-11",
-        isCarrierCell && shipData.carrier.shipColorClass,
-        isBattleshipCell && shipData.battleship.shipColorClass,
-        isDestroyerCell && shipData.destroyer.shipColorClass,
-        isSubmarineCell && shipData.submarine.shipColorClass,
-        isPatrollerCell && shipData.patroller.shipColorClass,
-        isMouseOver && isMissedHitCell && "cursor-not-allowed",
-        isMouseOver && isMissedHitCell && "bg-red-500",
-        isMouseOver && !isMissedHitCell && "bg-emerald-600",
+        isCarrierCell && shipIsAlreadySunk && shipData.carrier.shipColorClass,
+        isBattleshipCell &&
+          shipIsAlreadySunk &&
+          shipData.battleship.shipColorClass,
+        isDestroyerCell &&
+          shipIsAlreadySunk &&
+          shipData.destroyer.shipColorClass,
+        isSubmarineCell &&
+          shipIsAlreadySunk &&
+          shipData.submarine.shipColorClass,
+        isPatrollerCell &&
+          shipIsAlreadySunk &&
+          shipData.patroller.shipColorClass,
+        isMouseOver && isHitCell && "cursor-not-allowed",
+        isMouseOver && !isHitCell && "bg-emerald-500",
       )}
       onMouseEnter={() => setMouseOverCoords({ row, col })}
       onMouseLeave={() => setMouseOverCoords({ row: null, col: null })}
